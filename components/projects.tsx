934b2fad821dcf923d0aba39fcf7468e58ca24b2
Omor Faruk Rony <omorfarukrony2@gmail.com>
2025-04-22 20:56:44 +0600

    Fix resume download link in Intro component

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,13 +5,14 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import { BsGithub } from "react-icons/bs";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>My  Most Successful Projects</SectionHeading>
+      <SectionHeading>Some of My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
@@ -19,6 +20,16 @@ export default function Projects() {
           </React.Fragment>
         ))}
       </div>
+      <p className="text-center mt-10 text-gray-500 flex justify-center items-center gap-2">
+        You can find more on my GitHub profile.
+        <a
+          href="https://github.com/mr-rony356"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsGithub />
+        </a>
+      </p>
     </section>
   );
 }
